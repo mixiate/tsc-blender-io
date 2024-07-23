032dae916fd095150fb9c9777cbed22502d6bcbb
mix <mixsims@proton.me>
2024-07-23 21:57:54 +0100

    fix: add pets/castaway to shader alpha check list

--- addons/io_scene_tsc/import_shader.py
+++ addons/io_scene_tsc/import_shader.py
@@ -97,7 +97,12 @@ def import_shader(
             match game_type:
                 case utils.GameType.THESIMS | utils.GameType.THESIMSBUSTINOUT:
                     has_alpha = render_pass.flags & 0x4 != 0
-                case utils.GameType.THEURBZ | utils.GameType.THESIMS2:
+                case (
+                    utils.GameType.THEURBZ
+                    | utils.GameType.THESIMS2
+                    | utils.GameType.THESIMS2PETS
+                    | utils.GameType.THESIMS2CASTAWAY
+                ):
                     has_alpha = render_pass.raster_modes & 0x40 != 0 or render_pass.flags & 0x4 != 0
                 case _:
                     has_alpha = False
